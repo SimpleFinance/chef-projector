7e27ef9a4c2d5947d123ba3de583af2e34f1cbe8
Will Maier <wcmaier@m.aier.us>
2013-05-29 19:05:55 +0000

    Stab in resource + provider.

---
+++ libraries/provider_projector_job.rb
@@ -0,0 +1,107 @@
+require 'chef/provider'
+
+class Chef
+  class Provider
+    class ProjectorJob < Chef::Provider
+
+      def initialize(*args)
+        super
+        @command = nil
+        @config_dir = nil
+        @config_template = nil
+        @job = nil
+      end
+
+      def whyrun_supported?
+        # XXX
+        false
+      end
+
+      def load_current_resource
+        Chef::Resource::ProjectorJob.new(@new_resource.name)
+        @current_resource
+      end
+
+      def action_create
+        config_dir.run_action(:create)
+        config_template.run_action(:create)
+        job.run_action(:create)
+
+        updated = config_template.updated_by_last_action?
+        new_resource.updated_by_last_action(updated)
+      end
+      
+      def action_update
+        config_dir.run_action(:create)
+        config_template.run_action(:create)
+        updated = config_template.updated_by_last_action?
+        if updated
+          job.run_action(:create)
+        end
+        new_resource.updated_by_last_action(updated)
+      end
+
+      def action_delete
+        config_template.run_action(:delete)
+        config_dir.run_action(:delete)
+        updated = config_template.updated_by_last_action?
+        if updated
+          job.run_action(:delete)
+        end
+        new_resource.updated_by_last_action(updated)
+      end
+
+      def config_dir_name
+        ::File.join(new_resource.config_dir, new_resource.job_name)
+      end
+
+      def config_dir
+        return @config_dir unless @config_dir.nil?
+        
+        @config_dir = Chef::Resource::Directory.new(config_dir_name, run_context)
+
+        @config_dir.recursive(true)
+        @config_dir.owner(new_resource.owner)
+        @config_dir.group(new_resource.group)
+        @config_dir.mode(00700)
+
+        @config_dir
+      end
+
+      def config_template_name
+        ::File.join(config_dir_name, 'config.xml')
+      end
+
+      def config_template
+        return @config_template unless @config_template.nil?
+
+        @config_template = Chef::Resource::Template.new(config_template_name, run_context)
+        @config_template.cookbook(new_resource.cookbook)
+        @config_template.source(new_resource.template)
+
+        variables = new_resource.variables.merge({
+                      :command => @command,
+                      :description => @description,
+                      :target => @target,
+                      :repository => @repository,
+                      :branch => @branch
+                    })
+        @config_template.variables(variables)
+        @config_template.owner(new_resource.owner)
+        @config_template.group(new_resource.group)
+        @config_template.mode(00400)
+
+        @config_template
+      end
+
+      def job
+        return @job unless @job.nil?
+
+        @job = Chef::Resource::JenkinsJob.new(new_resource.job_name, run_context)
+        @job.config(config_template_name)
+        @job.url(new_resource.url)
+
+        @job
+      end
+    end
+  end
---
+++ libraries/resource_projector_job.rb
@@ -0,0 +1,91 @@
+require 'chef/resource'
+
+class Chef
+  class Resource
+    class ProjectorJob < Chef::Resource
+
+      identity_attr :job_name
+      provides :projector_job, :on_platforms => :all
+
+      def initialize(name, run_context=nil)
+        super
+        defaults = attrs_from_node(run_context)
+        @resource_name = :projector_job
+        @provider = Chef::Provider::ProjectorJob
+        @action = :create
+        
+        @command = defaults['command'] || 'make'
+        @target = defaults['target'] || 'build'
+        @owner = defaults['owner'] || 'projector'
+        @group = defaults['group'] || 'projector'
+        @template = defaults['template'] || 'config.xml.erb'
+        @branch = defaults['branch'] || 'master'
+        @variables = Hash.new
+        @description = ''
+        @cookbook = nil
+        @url = nil
+        @repository = nil
+
+        @job_name = "#{name}-#{@target}"
+      end
+
+      def command(arg=nil)
+        set_or_return(:command, arg, :kind_of => [String])
+      end
+
+      def owner(arg=nil)
+        set_or_return(:owner, arg, :kind_of => [String])
+      end
+
+      def group(arg=nil)
+        set_or_return(:group, arg, :kind_of => [String])
+      end
+
+      def description(arg=nil)
+        set_or_return(:description, arg, :kind_of => [String])
+      end
+
+      def template(arg=nil)
+        set_or_return(:template, arg, :kind_of => [String])
+      end
+
+      def variables(arg=nil)
+        set_or_return(:variables, arg, :kind_of => [Hash])
+      end
+
+      def cookbook(arg=nil)
+        set_or_return(:cookbook, arg, :kind_of => [String])
+      end
+
+      def job_name(arg=nil)
+        set_or_return(:job_name, arg, :kind_of => [String])
+      end
+
+      def target(arg=nil)
+        set_or_return(:target, arg, :kind_of => [String])
+      end
+
+      def url(arg=nil)
+        set_or_return(:url, arg, :kind_of => [String])
+      end
+
+      def repository(arg=nil)
+        set_or_return(:repository, arg, :kind_of => [String])
+      end
+
+      def branch(arg=nil)
+        set_or_return(:branch, arg, :kind_of => [String])
+      end
+
+      private
+
+      def attrs_from_node(run_context)
+        attrs = if run_context && run_context.node
+                  run_context.node[:projector]
+                end
+        attrs || {}
+      end
+
+    end
+  end
+end
