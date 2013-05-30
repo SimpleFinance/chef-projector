c3e9d851dc329338ab9101cb140478bc21a5748d
Will Maier <wcmaier@m.aier.us>
2013-05-30 00:27:01 +0000

    repo stuff.

---
+++ libraries/provider_projector_repository.rb
@@ -0,0 +1,114 @@
+require 'chef/provider'
+
+class Chef
+  class Provider
+    class ProjectorRepository < Chef:: Provider
+
+      def initialize(*args)
+        super
+      end
+
+      def whyrun_supported?
+        # XXX
+        false
+      end
+
+      def load_current_resource
+        @current_resource = Chef::Resource::ProjectorRepository.new(@new_resource.name)
+        repo = @client.repository(@new_resource.repo)
+        @current_resource.exists(repo ? true : false)
+        if repo
+          @current_resource.repo(repo.slug)
+          @current_resource.description(repo.description)
+          hooks = {}
+          @client.hooks.each do |hook|
+            hooks[hook.delete('name')] = hook
+          end
+          @current_resource.hooks(hooks)
+        end
+        
+        @current_resource
+      end
+
+      def action_create
+        if !@current_resource.exists
+          @client.create_repository(@new_resource.name,
+                                    :organization => @new_resource.org,
+                                    :description => @new_resource.description
+                                    )
+
+          @new_resource.hooks.each_pair do |service, config|
+            @client.create_hook(@new_resource.repo, service, config)
+          end
+        end
+        @new_resource.updated_by_last_action(true)
+      end
+
+      def action_update
+        updated = false
+        
+        if !@current_resource.exists
+          action_create
+          updated = true
+        end
+
+        options = {}
+        %w{name description}.each do |attr|
+          attr = attr.to_sym
+          current = @current_resource.send(attr)
+          new = @new_resource.send(attr)
+          options[attr] = new if current != new
+        end
+
+        if !options.empty?
+          # Use current_resource.repo here in case the name changes.
+          @client.edit_repository(@current_resource.repo, options)
+          updated = true
+        end
+
+        # Use new_resource.repo from here on out in case the name changed.
+        updated_hooks = []
+        @current_resource.hooks.each_pair do |service, hook|
+          config = @new_resource.hooks[service]
+          if config.nil?
+            @client.remove_hook(@new_resource.repo, hook['id'])
+            updated_hooks.push(service)
+          else
+            diff = config.select{|k,v| (v, nil).include?(hook[k])}
+            if !diff.empty?
+              @client.edit_hook(@new_resource.repo, id, service, config)
+              updated_hooks.push(service)
+            end
+          end
+        end
+        
+        @new_resource.hooks.each_pair do |service, config|
+          next if updated_hooks.include?(service)
+          @client.create_hook(@new_resource.repo, service, config)
+          updated_hooks.push(service)
+        end
+
+        updated ||= !updated_hooks.empty?
+
+        @new_resource.updated_by_last_action(updated)
+      end
+      
+      def action_delete
+        @client.delete_repository(@new_resource.repo)
+      end
+
+      def client
+        return @client unless client.nil?
+        
+        conn = new_resource.connection.dup
+        Octokit.configure do |c|
+          c.api_endpoint = conn.delete('api_endpoint') || 'https://api.github.com/api/v3'
+          c.web_endpoint = conn.delete('web_endpoint') || 'https://github.com/'
+        end
+
+        @client = Octokit::Client.new(**conn)
+      end
+
+    end
+  end
+end
---
+++ libraries/resource_projector_repository.rb
@@ -0,0 +1,77 @@
+require 'chef/resource'
+
+class Chef
+  class Resource
+    class ProjectorRepository < Chef::Resource
+
+      identity_attr :repository_path
+      provides :projector_repository, :on_platforms => :all
+
+      def initialize(name, run_context=nil)
+        super
+        defaults = attrs_from_node(run_context)
+        
+        @resource_name = :projector_repository
+        @provider = Chef::Provider::ProjectorRepository
+        @action = :create
+
+        @org = defaults['org']
+        @user = nil
+        @owner = @user || @org
+        @repo = "#{owner}/#{name}"
+        @description = ''
+        @connection = defaults['connection'] || {}
+        # XXX
+        @queue = defaults['queue'] || {}
+        @hooks = defaults['hooks'] || {}
+        @exists = false
+      end
+
+      def repo(arg=nil)
+        set_or_return(:repository_path, arg, :kind_of => [String])
+      end
+
+      def org(arg=nil)
+        set_or_return(:org, arg, :kind_of => [String])
+      end
+
+      def user(arg=nil)
+        set_or_return(:user, arg, :kind_of => [String])
+      end
+
+      def owner(arg=nil)
+        set_or_return(:owner, arg, :kind_of => [String])
+      end
+
+      def description(arg=nil)
+        set_or_return(:description, arg, :kind_of => [String])
+      end
+
+      def connection(arg=nil)
+        set_or_return(:connection, arg, :kind_of => [Hash])
+      end
+
+      def queue(arg=nil)
+        set_or_return(:queue, arg, :kind_of => [Hash])
+      end
+
+      def hooks(arg=nil)
+        set_or_return(:hooks, arg, :kind_of => [Hash])
+      end
+
+      def exists(arg=nil)
+        set_or_return(:exists, arg, :kind_of => [Boolean])
+      end
+
+      private
+
+      def attrs_from_node(run_context)
+        attrs = if run_context && run_context.node
+                  run_context.node[:projector]
+                end
+        (attrs || {})[:repository]
+      end
+
+    end
+  end
+end
